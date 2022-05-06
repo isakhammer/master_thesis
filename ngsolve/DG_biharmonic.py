ddd659f38582460579512eadd41ef7d52dba8f17
isakhammer <isakhammer@gmail.com>
2022-05-06 22:46:29 +0200

    Added ngsolve

--- ngsolve/DG_biharmonic.py
+++ ngsolve/DG_biharmonic.py
@@ -5,8 +5,7 @@ from ngsolve import *
 from netgen.geom2d import unit_square
 
 def poission_DG():
-    # https://docu.ngsolve.org/v6.2.1802/i-tutorials/unit-2.8-DG/DG.html
-
+    # https://docu.ngsolve.org/v6.2.1808/i-tutorials/unit-2.8-DG/DG.html
     mesh = Mesh (unit_square.GenerateMesh(maxh=0.1))
     order=4
     fes = L2(mesh, order=order, dgjumps=True)
@@ -63,10 +62,13 @@ def biharmonic_DG():
     alpha = 4
     h = specialcf.mesh_size
     a = BilinearForm(fes)
-    a += SymbolicBFI(grad(u)*grad(v))
+    a += SymbolicBFI(alpha*u*v)
+    a += SymbolicBFI(hesse(u)*hesse(v))
+
     a += SymbolicBFI(alpha*order**2/h*jump_u*jump_v, skeleton=True)
-    a += SymbolicBFI(alpha*order**2/h*u*v, BND, skeleton=True)
     a += SymbolicBFI(-mean_dudn*jump_v -mean_dvdn*jump_u, skeleton=True)
+
+    a += SymbolicBFI(alpha*order**2/h*u*v, BND, skeleton=True)
     a += SymbolicBFI(-n*grad(u)*v-n*grad(v)*u, BND, skeleton=True)
     a.Assemble()
 
