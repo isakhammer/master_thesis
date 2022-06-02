3cf234c0d48b62753df7b7ee6fefb30e0d67118a
isakhammer <isakhammer@gmail.com>
2022-06-02 22:42:04 +0200

    Added poission formulation

--- julia/DG_biharmonic.jl
+++ julia/DG_biharmonic.jl
@@ -2,72 +2,10 @@ using Gridap
 import Gridap: ∇
 
 
-function main2()
-    # source: https://gridap.github.io/Tutorials/v0.5/pages/t005_dg_discretization/
-    u(x) = 3*x[1] + x[2]^2
-    f(x) = -2 - 12*x[1]
-    g(x) = u(x)
-
-    # ∇u(x) = VectorValue(3   + x[2]*x[3],
-    #                 2*x[2]   + x[1]*x[3],
-    #                 6*x[3]^2 + x[1]*x[2])
-
-
-    println(∇(u) === ∇u)
-
-    # mesh generation
-    L = 1.0
-    n = 4
-    order = 2
-
-    domain2D = (0.0, L, 0.0, L)
-    partition2D = (n,n)
-    model = CartesianDiscreteModel(domain2D,partition2D)
-    writevtk(model,"model_mesh")
-
-    V = TestFESpace(model, ReferenceFE(lagrangian,Float64,order), conformity=:L2)
-    U = TrialFESpace(V)
-
-    Ω = Triangulation(model)
-    Γ = BoundaryTriangulation(model)
-    Λ = SkeletonTriangulation(model)
-
-    writevtk(Λ,"strian_cube")
-
-    degree = 2*order
-    dΩ = Measure(Ω,degree)
-    dΓ = Measure(Γ,degree)
-    dΛ = Measure(Λ,degree)
-
-    n_Γ = get_normal_vector(Γ)
-    n_Λ = get_normal_vector(Λ)
-
-    a_Ω(u,v) = ∫( ∇(v)⊙∇(u) )dΩ
-    l_Ω(v) = ∫( v*f )dΩ
-
-    h = L / n
-    γ = order*(order+1)
-    a_Γ(u,v) = ∫( - v*(∇(u)⋅n_Γ) - (∇(v)⋅n_Γ) * u + (γ/h)*v*u )dΓ
-    l_Γ(v)   = ∫(                - (∇(v)⋅n_Γ)*g + (γ/h)*v*g )dΓ
-
-    a_Λ(u,v) = ∫( - jump(v*n_Λ)⊙mean(∇(u))
-              - mean(∇(v))⊙jump(u*n_Λ)
-              + (γ/h)*jump(v*n_Λ)⊙jump(u*n_Λ) )dΛ
-
-    a(u,v) = a_Ω(u,v) + a_Γ(u,v) + a_Λ(u,v)
-    l(v) = l_Ω(v) + l_Γ(v)
-
-    op = AffineFEOperator(a, l, U, V)
-    uh = solve(op)
-    writevtk(Λ, "jumps", cellfields=["jump_u"=>jump(uh)])
-    writevtk(Γ, "shell", cellfields=["uh"=>uh])
-    writevtk(Ω, "field", cellfields=["uh"=>uh])
-
-end
 
 function main()
     f(x) = 1
-    g(x) = u(x)
+    g(x) = 0
 
     # ∇u(x) = VectorValue(3   + x[2]*x[3],
     #                 2*x[2]   + x[1]*x[3],
@@ -121,7 +59,6 @@ function main()
               + (γ/h)⋅ mean(∇(v) ⋅ n_Λ)⊙jump(∇(u)⋅ n_Λ)
                 )dΛ
 
-
     # Summation
     a(u,v) = a_Ω(u,v) + a_Λ(u,v)
     l(v) = l_Ω(v) + l_Γ(v)
@@ -134,5 +71,5 @@ function main()
 end
 
 
-main2()
-# main()
+# main2()
+main()
---
+++ julia/DG_poission.jl
@@ -0,0 +1,64 @@
+using Gridap
+
+function main2()
+    # source: https://gridap.github.io/Tutorials/v0.5/pages/t005_dg_discretization/
+    u(x) = 3*x[1] + x[2]^2
+    f(x) = -2 - 12*x[1]
+    g(x) = u(x)
+
+    ∇u(x) = VectorValue(3   ,
+                        2*x[2] )
+
+    # mesh generation
+    L = 1.0
+    n = 4
+    order = 2
+
+    domain2D = (0.0, L, 0.0, L)
+    partition2D = (n,n)
+    model = CartesianDiscreteModel(domain2D,partition2D)
+    writevtk(model,"plots/possion_model_mesh")
+
+    V = TestFESpace(model, ReferenceFE(lagrangian,Float64,order), conformity=:L2)
+    U = TrialFESpace(V)
+
+    Ω = Triangulation(model)
+    Γ = BoundaryTriangulation(model)
+    Λ = SkeletonTriangulation(model)
+
+    writevtk(Λ,"plots/Skeleton_possion_square")
+
+    degree = 2*order
+    dΩ = Measure(Ω,degree)
+    dΓ = Measure(Γ,degree)
+    dΛ = Measure(Λ,degree)
+
+    n_Γ = get_normal_vector(Γ)
+    n_Λ = get_normal_vector(Λ)
+
+    a_Ω(u,v) = ∫( ∇(v)⊙∇(u) )dΩ
+    l_Ω(v) = ∫( v*f )dΩ
+
+    h = L / n
+    γ = order*(order+1)
+    a_Γ(u,v) = ∫( - v*(∇(u)⋅n_Γ) - (∇(v)⋅n_Γ) * u + (γ/h)*v*u )dΓ
+    l_Γ(v)   = ∫(                - (∇(v)⋅n_Γ)*g + (γ/h)*v*g )dΓ
+
+
+    a_Λ(u,v) = ∫( - jump(v*n_Λ)⊙mean(∇(u))
+              - mean(∇(v))⊙jump(u*n_Λ)
+              + (γ/h)*jump(v*n_Λ)⊙jump(u*n_Λ) )dΛ
+
+    a(u,v) = a_Ω(u,v) + a_Γ(u,v) + a_Λ(u,v)
+    l(v) = l_Ω(v) + l_Γ(v)
+
+    op = AffineFEOperator(a, l, U, V)
+    uh = solve(op)
+    writevtk(Λ, "plots/possion_jumps", cellfields=["jump_u"=>jump(uh)])
+    writevtk(Γ, "plots/possion_shell", cellfields=["uh"=>uh])
+    writevtk(Ω, "plots/possion_field", cellfields=["uh"=>uh])
+
+end
+
+main2()
+
