68717cfca710597db6ccf6ddf5528d9e633e8c78
isakhammer <isakhammer@gmail.com>
2022-06-16 15:11:19 +0200

    cahn hilliard with custom b.c. runs

--- julia/cahn_hilliard.jl
+++ julia/cahn_hilliard.jl
@@ -10,17 +10,36 @@ function run_cahn_hilliard(; n=10, order::Int, generate_vtk=false, dirname="cahn
 
     # Analytical manufactured solution
     α = 1
-    u(x) = cos(x[1])*cos(x[2])
-    f(x) = Δ(Δ(u))(x)+ α*u(x)
-    g(x) = Δ(u)(x)
+    u(x,t::Real) = t*cos(x[1])*cos(x[2])
+    u(t::Real) = x -> u(x,t)
+
+    # f(t::Real) = x -> Δ(Δ(u(t)))(x)+ α*∂t(u)
+    # g(t::Real) = x -> Δ(u(t))(x)
+
+    # f(x, t::Real) =  1
+    # g(x, t::Real) =  0
+    # f(t) = x -> ∂t(u)(x,t)-Δ(u(t))(x)
+
+    f(t) = x -> α*∂t(u)(x,t)+Δ(Δ(u(t)))(x)
+    g(t) = x ->  Δ(u(t))(x)
+
+    # Manufactured solution test
+    # tₜ, x1ₜ, x2ₜ  = 0.5, 0.5, 0.5
+    # @test f(tₜ)(VectorValue(x1ₜ,x2ₜ)) ==  4*tₜ*cos(x1ₜ)*cos(x2ₜ) + cos(x1ₜ)*cos(x2ₜ)
+    # @test g(tₜ)(VectorValue(x1ₜ,x2ₜ)) == -2*tₜ*cos(x1ₜ)*cos(x2ₜ)
+
+    # u(x,t) = (1.0-x[1])*x[1]*(1.0-x[2])*x[2]*t
+    # u(t::Real) = x -> u(x,t)
+    # f(t) = x -> ∂t(u)(x,t)-Δ(u(t))(x)
+
+
+    # u(x) = cos(x[1])*cos(x[2])
+    # f(x) = Δ(Δ(u))(x)+ α*u(x)
+    # g(x) = Δ(u)(x)
 
 
     # Domain
     L = 2*π
-
-    @test f(VectorValue(0.5,0.5)) == ( 4+α )*u(VectorValue(0.5,0.5))
-    @test g(VectorValue(0.5,0.5)) == -2*u(VectorValue(0.5,0.5))
-
     domain = (0,L,0,L)
     partition = (n,n)
     model = CartesianDiscreteModel(domain,partition)
@@ -52,7 +71,7 @@ function run_cahn_hilliard(; n=10, order::Int, generate_vtk=false, dirname="cahn
                 ∫( - mean(Δ(u))*jump(∇(v)⋅n_Λ) - jump(∇(u)⋅n_Λ)*mean(Δ(v))
                     + γ/h*jump(∇(u)⋅n_Λ)*jump(∇(v)⋅n_Λ) )dΛ
 
-    b(t, v) = ∫( v*f )dΩ + ∫( g*(∇(v)⋅n_Γ) )dΓ
+    b(t, v) = ∫( v*f(t) )dΩ + ∫( g(t)*(∇(v)⋅n_Γ) )dΓ
 
     # Initializing linear terms
     op_Af = TransientAffineFEOperator(m,a,b,U,V)
@@ -73,6 +92,7 @@ function run_cahn_hilliard(; n=10, order::Int, generate_vtk=false, dirname="cahn
     # op = op_AD
     t_0=0
     T=3
+
     U_h_t = solve(ode_solver, op, U_0, t_0, T)
 
     solname = dirname*"/sol"
@@ -80,8 +100,14 @@ function run_cahn_hilliard(; n=10, order::Int, generate_vtk=false, dirname="cahn
         for (U_h, t) in U_h_t
             println("t "*string(t))
             pvd[t] = createvtk(Ω, solname*"_$t"*".vtu",cellfields=["U_h"=>U_h])
+
+            # e = u(tn) - uh_tn
+            # el2 = sqrt(sum( ∫(l2(e))dΩ ))
+            # @test el2 < tol
         end
     end
+
+
 
 
 
