6225bad3f4ecb49853af3e861302f0e03de546d9
isakhammer <isakhammer@gmail.com>
2023-01-30 22:55:35 +0100

    Added cleanup on nitsche

--- master_thesis/julia/poisson_nitsche.jl
+++ master_thesis/julia/poisson_nitsche.jl
@@ -1,138 +1,198 @@
-##
-using Gridap
+include("results.jl")
 
-# Define manufactured solution
-u(x) = 100*cos(x[1])*cos(x[2])
+module PoissonNitsche
+    using Gridap
+    using Parameters
+    import GridapMakie
+    import Makie
+    import GLMakie
+    using Test
 
-# -Δu = f
-f(x) = 2*u(x)
-g(x) = u(x)
 
-# ∂Δu/∂n
-# q(x) = 0
+    @with_kw struct Results
+        Ω
+        Γ
+        Λ
 
-function run_exp(n=8, k=2, use_quads=false)
-    ## Mesh
-    pmin = Point(0.,0.0)
-    pmax = Point(2π, 2π)
-    partition = (n, n)
+        model
+        h::Real
 
-    if !use_quads
-        model = CartesianDiscreteModel(pmin, pmax, partition) |> simplexify
-    else
-        model = CartesianDiscreteModel(pmin, pmax, partition)
+        u
+        uh
+        e
+        el2
+        eh1
+        eh_energy
     end
 
-    # Define triangulation
-    Ω = Triangulation(model)
-    Γ = BoundaryTriangulation(model)
+    function generate_vtk(; res::Results, dirname::String)
+        println("Generating vtk's in ", dirname)
+        if (isdir(dirname))
+            rm(dirname, recursive=true)
+        end
+        mkdir(dirname)
 
-    # Write out meshes
-    folder = "figures/poisson_nitsche/"
-    mkpath(folder)
-    writevtk(model, folder*"model_n_$(n)")
-    writevtk(Ω, folder*"mesh_n_$(n)")
+        writevtk(res.model, dirname*"/model")
+        writevtk(res.Λ, dirname*"/skeleton")
+        writevtk(res.Γ, dirname*"/boundary")
+        writevtk(res.Λ, dirname*"/jumps",cellfields=["jump_u"=>jump(res.uh)])
+        writevtk(res.Ω, dirname*"/omega",cellfields=["uh"=>res.uh])
+        writevtk(res.Ω, dirname*"/error",cellfields=["e"=>res.e])
+        writevtk(res.Ω, dirname*"/manufatured",cellfields=["u"=>res.u])
 
-    ## Function spaces
-    order = k
-    reffe = ReferenceFE(lagrangian, Float64, order)
-    V = TestFESpace(Ω, reffe, conformity=:H1)
-    U = TrialFESpace(V)
 
-    ## Define the weak form
-    degree = 2*order
-    dΩ = Measure(Ω, degree)
-    dΓ= Measure(Γ, degree)
+        fig = Makie.plot(res.Λ)
+        Makie.wireframe!(res.Λ, color=:black, linewidth=2)
+        Makie.wireframe!(res.Γ, color=:black, linewidth=2)
+        Makie.save(dirname*"/grid.png", fig)
 
-    # Normal vectors
-    n_Γ  = get_normal_vector(Γ)
-
-    # Define mesh and stabilization parameters
-    h = norm((pmax-pmin)./VectorValue(partition))
-
-    # γ = 5.0*order*(order+1)  # Penalty parameter
-    γ = 5  # Penalty parameter
-    μ = γ/h
-
-    a_Ω(u,v) =∫( ∇(v)⋅∇(u) )dΩ
-    a_Γ(u,v) =∫( - ( ∇(u)⋅n_Γ )⊙v - u⊙( ∇(v)⋅n_Γ ) + μ*u⊙v )dΓ
-    a(u,v) = a_Ω(u,v) + a_Γ(u,v)
-
-    l_Ω(v) = ∫( v⊙f )dΩ
-    l_Γ(v) = ∫( -(( ∇(v)⋅n_Γ )⊙g) + μ*(g⊙v) )dΓ
-    l(v) = l_Ω(v) + l_Γ(v)
-
-    ## Solve and postprocess
-    op = AffineFEOperator(a, l, U, V)
-    uh = solve(op)
-
-    # Compute interpolation of exact solution for visual comparison
-    u_inter = interpolate_everywhere(u, V)
-
-    # Compute error
-    e = u - uh
-
-    outputfile = folder*"possion_nitsche_n_$(n)_k_$(k)_use_quads_$(use_quads)"
-    writevtk(Ω, outputfile, cellfields=["uh" => uh, "u_inter" => u_inter, "error" => e])
-
-    l2(u) = sqrt(sum( ∫( u⊙u )*dΩ ))
-    h1(u) = sqrt(sum( ∫( u⊙u + ∇(u)⊙∇(u) )*dΩ ))
-    energy(u) = sqrt(sum( ∫( u⊙u )*dΩ  + ∫( ( 1/h )*( u⊙u ) )*dΓ)) # Defined on eq 3.16 Sigmund project thesis
-
-    el2 = l2(e)
-    eh1 = h1(e)
-    eaₕ = energy(e)
-
-    println("h = $h, L2 error = $el2")
-    println("h = $h, H1 error = $eh1")
-
-    (h, el2, eh1, eaₕ)
-end
-
-function conv_test(ns, k, use_quads)
-
-    hs = Float64[]
-    el2s = Float64[]
-    eh1s = Float64[]
-    eaₕs = Float64[]
-
-    for n in ns
-        h, el2, eh1, eaₕ = run_exp(n, k, use_quads)
-        push!(hs, h)
-        push!(el2s, el2)
-        push!(eh1s, eh1)
-        push!(eaₕs, eaₕ)
+        # (Isak): Doesnt work :( Please fix
+        # fig, _ , plt = Makie.plot(res.Ω, res.uh)
+        # Makie.Colorbar(fig[1,2], plt)
+        # Makie.save(dirname*"/man_sol.png", fig)
     end
 
-    println("Mesh sizes = $hs")
-    println("L2 errors  = $el2s")
-    println("H1 errors  = $eh1s")
-    println("Energy errors  = $eaₕs")
 
-    # Compute eoc
-    (hs, el2s, eh1s, eaₕs)
-end
+    function run(; order=order, n=n, L=L,  m=m, r=r, use_quads=false)
+        # Some parameters
+        # h = set.L/set.n
+        # γ = 1.5*set.order*( set.order+1)
+        # domain2D = (0, set.L, 0, set.L)
+        # partition2D = (set.n, set.n)
+        # if !set.use_quads
+        #     model = CartesianDiscreteModel(domain2D,partition2D) |> simplexify
+        # else
+        #     model = CartesianDiscreteModel(domain2D,partition2D)
+        # end
 
-function compute_eoc(hs, errs)
-    eoc = log.(errs[1:end-1]./errs[2:end])./log.(hs[1:end-1]./hs[2:end])
+        ##
+
+        pmin = Point(0.,0.0)
+        pmax = Point(L, L)
+        partition = (n, n)
+
+        if !use_quads
+            model = CartesianDiscreteModel(pmin, pmax, partition) |> simplexify
+        else
+            model = CartesianDiscreteModel(pmin, pmax, partition)
+        end
+        h = norm((pmax-pmin)./VectorValue(partition))
+        ##
+
+        # u is the manufactured solution
+        # -Δu = f in Ω, and u = g on Γ
+        # u = man_sol(L=set.L, m=set.m, r=set.r)
+        u(x) = 100*cos(x[1])*cos(x[2])
+        f(x) = 2*u(x)
+        g(x) = u(x)
+
+        # Define triangulation
+        Ω = Triangulation(model)
+        Γ = BoundaryTriangulation(model)
+        Λ = SkeletonTriangulation(model)
+
+        ## Function spaces
+        # order = 2
+        # reffe = ReferenceFE(lagrangian, Float64, order)
+        reffe = ReferenceFE(lagrangian, Float64, order)
+
+        V = TestFESpace(Ω, reffe, conformity=:H1)
+        U = TrialFESpace(V)
+
+        ## Define the weak form
+        degree = 2*order
+        dΩ = Measure(Ω, degree)
+        dΓ= Measure(Γ, degree)
+
+        n_Γ  = get_normal_vector(Γ)
+
+        # Define mesh and stabilization parameters
+        h = norm((pmax-pmin)./VectorValue(partition))
+
+        # γ = 5.0*order*(order+1)  # Penalty parameter
+        γ = 5  # Penalty parameter
+        μ = γ/h
+
+        a_Ω(u,v) =∫( ∇(v)⋅∇(u) )dΩ
+        a_Γ(u,v) =∫( - ( ∇(u)⋅n_Γ )⊙v - u⊙( ∇(v)⋅n_Γ ) + μ*u⊙v )dΓ
+        a(u,v) = a_Ω(u,v) + a_Γ(u,v)
+
+        l_Ω(v) = ∫( v⊙f )dΩ
+        l_Γ(v) = ∫( -(( ∇(v)⋅n_Γ )⊙g) + μ*(g⊙v) )dΓ
+        l(v) = l_Ω(v) + l_Γ(v)
+
+
+        op = AffineFEOperator(a, l, U, V)
+        uh = solve(op)
+
+        e = u - uh
+        el2 = sqrt(sum( ∫(e*e)dΩ ))
+        eh_energy = sqrt(sum( ∫(∇(e)⊙∇(e) )*dΩ ))
+        eh1 = sqrt(sum( ∫( e⊙e + ∇(e)⊙∇(e) )*dΩ ))
+
+        u_inter = interpolate(u, V)
+        res = Results(  model=model, Ω=Ω, Γ=Γ, Λ=Λ, h=h,
+                        u=u_inter, uh=uh, e=e, el2=el2, eh1=eh1, eh_energy=eh_energy)
+        return res
+    end
+
+end # module
+
+
+
+
+
+
+
+function convergence_analysis(; L, m, r, orders, ns, dirname, optimize)
+    println("Run convergence",)
+
+    for order in orders
+
+        el2s = Float64[]
+        eh1s = Float64[]
+        ehs_energy = Float64[]
+        println("Run convergence tests: order = "*string(order))
+
+        for n in ns
+
+            res = PoissonNitsche.run(order=order, n=n, L=L, m=m, r=r)
+
+            if !(optimize)
+                vtkdirname =dirname*"/order_"*string(order)*"_n_"*string(n)
+                mkpath(vtkdirname)
+                PoissonNitsche.generate_vtk(res=res, dirname=vtkdirname)
+            end
+
+            push!(el2s, res.el2)
+            push!(eh1s, res.eh1)
+            push!(ehs_energy, res.eh_energy)
+        end
+        Results.generate_figures(ns=ns, el2s=el2s, eh1s=eh1s, ehs_energy=ehs_energy, order=order, dirname=dirname)
+    end
+
 end
 
 function main()
-    use_quads = false
-    ks  = [2, 3, 4]
-    ns = [4, 8, 16, 32, 64, 128]
-    for k in ks
-        println("Running EOC test for k = $k")
-        println("========================================")
-        hs, el2s, eh1s, eaₕs = conv_test(ns, k, use_quads)
-        eoc_l2 = compute_eoc(hs, el2s)
-        eoc_h1 = compute_eoc(hs, eh1s)
-        eoc_energy = compute_eoc(hs, eaₕs)
-        println("L2 EOC = $eoc_l2")
-        println("H1 EOC = $eoc_h1")
-        println("Energy EOC = $eoc_energy")
-        println("========================================")
+    function makedir(dirname)
+        if (isdir(dirname))
+            rm(dirname, recursive=true)
+        end
+        mkdir(dirname)
     end
+
+    resultdir= "figures/poisson_nitsche/"*string(Dates.now())
+    mkpath(resultdir)
+
+    function run(;  L,m,r)
+        orders=[2,3,4]
+        ns = [2^2, 2^3, 2^4, 2^5]#, 2^6, 2^7]
+        dirname = resultdir*"/L_"*string(round(L,digits=2))*"_m_"*string(m)*"_r_"*string(r);
+        makedir(dirname)
+        convergence_analysis( L=L, m=m, r=r, orders=orders, ns=ns, dirname=dirname, optimize=true)
+    end
+    run(L=1,m=1,r=1)
 end
 
-main()
+@time main()
+
