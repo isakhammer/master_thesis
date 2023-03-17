a5bd14cdeeb84e2110e08afa13de51f7df4d929a
isakhammer <isakhammer@gmail.com>
2023-03-17 22:06:27 +0100

    Added requirements

---
+++ master_thesis/julia/requirements.jl
@@ -0,0 +1,21 @@
+
+using Pkg
+Pkg.add([
+"BenchmarkTools",
+"Parameters",
+"LaTeXStrings",
+"Latexify",
+"Plots",
+"IJulia",
+"Gridap",
+"GridapMakie",
+"GridapEmbedded",
+"GridapGmsh",
+"TexTables",
+"PrettyTables",
+"DataStructures",
+"DataFrames",
+"Symbolics",
+"CairoMakie",
+"GLMakie",
+"FileIO"])
