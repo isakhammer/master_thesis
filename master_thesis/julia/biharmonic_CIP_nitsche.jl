a95c9810def5edd6eafcce2f403bec56b7404844
isakhammer <isakhammer@gmail.com>
2023-03-19 18:29:06 +0100

    Fixed ndofs

--- master_thesis/julia/biharmonic_CIP_nitsche.jl
+++ master_thesis/julia/biharmonic_CIP_nitsche.jl
@@ -14,7 +14,7 @@ module Solver
     using Parameters
     import Gridap: ∇
 
-    function compute_condition_number(A; method="inf")
+    function compute_condition_number(A, ndof; method="inf")
 
         if method == "svd"
             tolin = 10^-8
@@ -25,9 +25,7 @@ module Solver
             return s_max[1] / s_min
         elseif method == "inf"
             # https://github.com/mfasi/julia/blob/4ceb4ea9ee46ea92d406cfced308451a112d16f9/base/sparse/linalg.jl#L505
-            ndofs = size(A)[1]
-            println( "ndofs = ", ndofs)
-            return ( 1/sqrt(ndofs) )*cond(A,Inf)
+            return ( 1/sqrt(ndof) )*cond(A,Inf)
         else method == "1"
             return cond(A,p=1)
         end
@@ -67,6 +65,7 @@ module Solver
         eh1
         eh_energy
         cond_number
+        ndof
     end
 
     function generate_vtk(; sol::Solution, vtkdirname::String)
@@ -137,7 +136,8 @@ module Solver
         op = AffineFEOperator(a, l, U, V)
         uh = solve(op)
         A =  get_matrix(op)
-        cond_number = compute_condition_number(A)
+        ndof = size(A)[1]
+        cond_number = compute_condition_number(A, ndof)
 
         e = u_ex - uh
         el2 = sqrt(sum( ∫(e*e)dΩ ))
@@ -157,7 +157,7 @@ module Solver
 
         sol = Solution(  model=model, Ω=Ω, Γ=Γ, Λ=Λ, h=h,
                         u=u_inter, uh=uh, e=e, el2=el2, eh1=eh1, eh_energy=eh_energy,
-                        cond_number=cond_number)
+                        cond_number=cond_number, ndof=ndof)
 
         if ( vtkdirname!=nothing)
             generate_vtk(sol=sol, vtkdirname=vtkdirname)
@@ -169,7 +169,7 @@ module Solver
 end # module
 
 
-function generate_figures(;ns, el2s, eh1s, ehs_energy, cond_numbers, order::Integer, dirname::String)
+function generate_figures(;ns, el2s, eh1s, ehs_energy, cond_numbers, ndofs, order::Integer, dirname::String)
     filename = dirname*"/conv_order_"*string(order)
 
     hs = 1 .// ns
@@ -190,13 +190,9 @@ function generate_figures(;ns, el2s, eh1s, ehs_energy, cond_numbers, order::Inte
         pretty_table(io, data, header=header, backend=Val(:latex ), formatters = ( ft_printf("%.3E"), ft_nonothing ))
     end
 
-    minimal_header = ["n", "L2", "EOC", "H1", "EOC", "a_h", "EOC", "cond", "const"]
-    data = hcat(ns, el2s,  eoc_l2, eh1s, eoc_eh1, ehs_energy, eoc_eh_energy, cond_numbers*hs^4)
-    pretty_table(data, header=minimal_header, formatters = ( ft_printf("%.1E",[2,4,6,8,9]), ft_nonothing ))
-    # open(filename*".txt", "w") do io
-    #     pretty_table(io, data, header=minimal_header, formatters = ( ft_printf("%.3E"), ft_nonothing ))
-    # end
-
+    minimal_header = ["n", "L2", "EOC", "H1", "EOC", "a_h", "EOC", "cond", "const","ndofs"]
+    data = hcat(ns, el2s,  eoc_l2, eh1s, eoc_eh1, ehs_energy, eoc_eh_energy, cond_numbers, cond_numbers.*hs.^4, ndofs)
+    pretty_table(data, header=minimal_header, formatters = ( ft_printf("%.0f",[1,10]), ft_printf("%.2f",[3,5,7]), ft_printf("%.1E",[2,4,6,8,9]), ft_nonothing ))
 
     # Plots.plot(hs, (el2s, eh1s, ehs_energy), label=(L"\\Vert e \\Vert_{L^2}", L"\\Vert e \\Vert_{ H^1 }^{  } ", L"\\Vert e  \\Vert_{ a_h,* }"))
     Plots.plot(hs, [el2s, eh1s, ehs_energy], label=[L" | e |_{L^2}", L"| e |_{ H^1 }^{  } ", L"| e  |_{ a_h,* }"])
@@ -219,6 +215,7 @@ function convergence_analysis(; orders, ns, dirname, write_vtks=true)
         eh1s = Float64[]
         ehs_energy = Float64[]
         cond_numbers = Float64[]
+        ndofs = Float64[]
         println("Run convergence tests: order = "*string(order))
 
         for n in ns
@@ -235,9 +232,10 @@ function convergence_analysis(; orders, ns, dirname, write_vtks=true)
             push!(eh1s, sol.eh1)
             push!(ehs_energy, sol.eh_energy)
             push!(cond_numbers, sol.cond_number)
+            push!(ndofs, sol.ndof)
         end
         generate_figures(ns=ns, el2s=el2s, eh1s=eh1s, ehs_energy=ehs_energy,
-                         cond_numbers=cond_numbers, order=order, dirname=dirname)
+                         cond_numbers=cond_numbers, order=order, ndofs=ndofs, dirname=dirname)
     end
 end
 
