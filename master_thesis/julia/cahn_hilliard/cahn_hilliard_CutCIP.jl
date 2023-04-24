1dd4fd7cad64563ff7e034bb685e64916b29c6fc
isakhammer <isakhammer@gmail.com>
2023-04-25 00:01:37 +0200

    Commenting out original code for now

--- master_thesis/julia/cahn_hilliard/cahn_hilliard_CutCIP.jl
+++ master_thesis/julia/cahn_hilliard/cahn_hilliard_CutCIP.jl
@@ -219,172 +219,171 @@ module Solver
 
 end # Solver
 
-function generate_figures(Xs,
-        el2s_L2, eh1s_L2, ehs_energy_L2,
-        el2s_inf, eh1s_inf, ehs_energy_inf,
-        dirname::String, Xs_name::String)
+# """
+# function generate_figures(Xs,
+#         el2s_L2, eh1s_L2, ehs_energy_L2,
+#         el2s_inf, eh1s_inf, ehs_energy_inf,
+#         dirname::String, Xs_name::String)
 
-    filename = dirname*"/conv_"*Xs_name
-    compute_eoc(Xs, errs) = log.(errs[1:end-1]./errs[2:end])./log.(Xs[1:end-1]./Xs[2:end])
-
-
-    Xs_str =  latexify.(Xs)
-
-    eoc_l2s_L2 = compute_eoc(Xs, el2s_L2)
-    eoc_eh1s_L2 = compute_eoc(Xs, eh1s_L2)
-    eoc_ehs_energy_L2 = compute_eoc(Xs, ehs_energy_L2)
-    eoc_l2s_inf = compute_eoc(Xs, el2s_inf)
-    eoc_eh1s_inf = compute_eoc(Xs, eh1s_inf)
-    eoc_ehs_energy_inf = compute_eoc(Xs, ehs_energy_inf)
-
-    eoc_l2s_L2 =  [nothing; eoc_l2s_L2]
-    eoc_eh1s_L2 =  [nothing; eoc_eh1s_L2]
-    eoc_ehs_energy_L2 =  [nothing; eoc_ehs_energy_L2]
-    eoc_l2s_inf =  [nothing; eoc_l2s_inf]
-    eoc_eh1s_inf =  [nothing; eoc_eh1s_inf]
-    eoc_ehs_energy_inf =  [nothing; eoc_ehs_energy_inf]
-
-    data = hcat(Xs_str,
-                el2s_L2,  eoc_l2s_L2, eh1s_L2, eoc_eh1s_L2, ehs_energy_L2, eoc_ehs_energy_L2,
-                el2s_inf,  eoc_l2s_inf, eh1s_inf, eoc_eh1s_inf, ehs_energy_inf, eoc_ehs_energy_inf)
-
-    formatters = ( ft_nonothing, ft_printf("%.2f", [3, 5, 7, 9, 11,13]),
-                  ft_printf("%.1E", [2, 4, 6, 8, 10,12]))
-
-    header = ["$Xs_name",
-              "L2L2", "EOC", "L2H1", "EOC", "L2ah", "EOC",
-              "infL2", "EOC", "infH1", "EOC", "infah", "EOC"]
-
-    pretty_table(data, header=header, formatters =formatters )
+#     filename = dirname*"/conv_"*Xs_name
+#     compute_eoc(Xs, errs) = log.(errs[1:end-1]./errs[2:end])./log.(Xs[1:end-1]./Xs[2:end])
 
 
-    formatters = ( ft_nonothing, ft_printf("%.5f", [3, 5, 7, 9, 11,13]),
-                  ft_printf("%.1E", [2, 4, 6, 8, 10,12]))
+#     Xs_str =  latexify.(Xs)
 
-    open(filename*".tex", "w") do io
-        pretty_table(io, data, header=header, backend=Val(:latex ), formatters = formatters )
-    end
+#     eoc_l2s_L2 = compute_eoc(Xs, el2s_L2)
+#     eoc_eh1s_L2 = compute_eoc(Xs, eh1s_L2)
+#     eoc_ehs_energy_L2 = compute_eoc(Xs, ehs_energy_L2)
+#     eoc_l2s_inf = compute_eoc(Xs, el2s_inf)
+#     eoc_eh1s_inf = compute_eoc(Xs, eh1s_inf)
+#     eoc_ehs_energy_inf = compute_eoc(Xs, ehs_energy_inf)
 
-    # L2 norms
-    p = Plots.plot(Xs, el2s_L2, label="L2L2", legend=:bottomright, xscale=:log2, yscale=:log2, minorgrid=true)
-    Plots.scatter!(p, Xs, el2s_L2, primary=false)
+#     eoc_l2s_L2 =  [nothing; eoc_l2s_L2]
+#     eoc_eh1s_L2 =  [nothing; eoc_eh1s_L2]
+#     eoc_ehs_energy_L2 =  [nothing; eoc_ehs_energy_L2]
+#     eoc_l2s_inf =  [nothing; eoc_l2s_inf]
+#     eoc_eh1s_inf =  [nothing; eoc_eh1s_inf]
+#     eoc_ehs_energy_inf =  [nothing; eoc_ehs_energy_inf]
 
-    Plots.plot!(p, Xs, eh1s_L2, label=L"L2H1")
-    Plots.scatter!(p, Xs, eh1s_L2, primary=false)
+#     data = hcat(Xs_str,
+#                 el2s_L2,  eoc_l2s_L2, eh1s_L2, eoc_eh1s_L2, ehs_energy_L2, eoc_ehs_energy_L2,
+#                 el2s_inf,  eoc_l2s_inf, eh1s_inf, eoc_eh1s_inf, ehs_energy_inf, eoc_ehs_energy_inf)
 
-    Plots.plot!(p, Xs, ehs_energy_L2, label=L"L2ah")
-    Plots.scatter!(p, Xs, ehs_energy_L2, primary=false)
+#     formatters = ( ft_nonothing, ft_printf("%.2f", [3, 5, 7, 9, 11,13]),
+#                   ft_printf("%.1E", [2, 4, 6, 8, 10,12]))
 
-    # inf norms
-    Plots.plot!(p, Xs, el2s_inf, label=L"infL2")
-    Plots.scatter!(p, Xs, el2s_inf, primary=false)
+#     header = ["$Xs_name",
+#               "L2L2", "EOC", "L2H1", "EOC", "L2ah", "EOC",
+#               "infL2", "EOC", "infH1", "EOC", "infah", "EOC"]
 
-    Plots.plot!(p, Xs, eh1s_inf, label=L"infH1")
-    Plots.scatter!(p, Xs, eh1s_inf, primary=false)
-
-    Plots.plot!(p, Xs, ehs_energy_inf, label=L"infah")
-    Plots.scatter!(p, Xs, ehs_energy_inf, primary=false)
-
-    # Configs
-    Plots.xlabel!(p, "$Xs_name")
-    Plots.plot!(p, xscale=:log2, yscale=:log2, minorgrid=true)
-    Plots.plot!(p, legendfontsize=12)  # Adjust the value 12 to your desired font size
-
-    # Save the plot as a .png file using the GR backend
-    # Plots.gr()
-    # Plots.pgfplotsx()
-    Plots.savefig(p, filename*"_plot.png")
-    # Plots.savefig(p, filename*"_plot.tex")
-
-end
-function convergence_analysis(; ns, dts, dirname, solver_config, spatial=false, dt_const=2^-4, transient=false, n_const=2^7)
-    println("Run convergence",)
-
-    if (transient)
-        # Time dim EOC
-        el2s_L2 = Float64[]
-        eh1s_L2 = Float64[]
-        ehs_energy_L2 = Float64[]
-        el2s_inf = Float64[]
-        eh1s_inf = Float64[]
-        ehs_energy_inf = Float64[]
-        println("Run convergence tests with constant n = "*string(n_const))
-
-        for dt in dts
-            sol = Solver.run(n=n_const, dt=dt, solver_config=solver_config, vtkdirname=dirname)
-
-            push!(el2s_L2, sol.el2s_L2)
-            push!(eh1s_L2, sol.eh1s_L2)
-            push!(ehs_energy_L2, sol.ehs_energy_L2)
-            push!(el2s_inf, sol.el2s_inf)
-            push!(eh1s_inf, sol.eh1s_inf)
-            push!(ehs_energy_inf, sol.ehs_energy_inf)
-        end
-        generate_figures(dts, el2s_L2, eh1s_L2, ehs_energy_L2,
-                         el2s_inf, eh1s_inf, ehs_energy_inf,
-                         dirname, "dt")
-    end
+#     pretty_table(data, header=header, formatters =formatters )
 
 
-    if (spatial)
-        # Spatial EOC
-        el2s_L2 = Float64[]
-        eh1s_L2 = Float64[]
-        ehs_energy_L2 = Float64[]
-        el2s_inf = Float64[]
-        eh1s_inf = Float64[]
-        ehs_energy_inf = Float64[]
-        println("Run convergence tests with constant dt = "*string(dt_const))
-        for n in ns
-            sol = Solver.run(n=n, dt=dt_const, solver_config=solver_config, vtkdirname=dirname)
-            push!(el2s_L2, sol.el2s_L2)
-            push!(eh1s_L2, sol.eh1s_L2)
-            push!(ehs_energy_L2, sol.ehs_energy_L2)
-            push!(el2s_inf, sol.el2s_inf)
-            push!(eh1s_inf, sol.eh1s_inf)
-            push!(ehs_energy_inf, sol.ehs_energy_inf)
-        end
-        hs =  1 .// ns
-        generate_figures(hs, el2s_L2, eh1s_L2, ehs_energy_L2,
-                         el2s_inf, eh1s_inf, ehs_energy_inf,
-                         dirname, "h")
-    end
+#     formatters = ( ft_nonothing, ft_printf("%.5f", [3, 5, 7, 9, 11,13]),
+#                   ft_printf("%.1E", [2, 4, 6, 8, 10,12]))
+
+#     open(filename*".tex", "w") do io
+#         pretty_table(io, data, header=header, backend=Val(:latex ), formatters = formatters )
+#     end
+
+#     # L2 norms
+#     p = Plots.plot(Xs, el2s_L2, label="L2L2", legend=:bottomright, xscale=:log2, yscale=:log2, minorgrid=true)
+#     Plots.scatter!(p, Xs, el2s_L2, primary=false)
+
+#     Plots.plot!(p, Xs, eh1s_L2, label=L"L2H1")
+#     Plots.scatter!(p, Xs, eh1s_L2, primary=false)
+
+#     Plots.plot!(p, Xs, ehs_energy_L2, label=L"L2ah")
+#     Plots.scatter!(p, Xs, ehs_energy_L2, primary=false)
+
+#     # inf norms
+#     Plots.plot!(p, Xs, el2s_inf, label=L"infL2")
+#     Plots.scatter!(p, Xs, el2s_inf, primary=false)
+
+#     Plots.plot!(p, Xs, eh1s_inf, label=L"infH1")
+#     Plots.scatter!(p, Xs, eh1s_inf, primary=false)
+
+#     Plots.plot!(p, Xs, ehs_energy_inf, label=L"infah")
+#     Plots.scatter!(p, Xs, ehs_energy_inf, primary=false)
+
+#     # Configs
+#     Plots.xlabel!(p, "$Xs_name")
+#     Plots.plot!(p, xscale=:log2, yscale=:log2, minorgrid=true)
+#     Plots.plot!(p, legendfontsize=12)  # Adjust the value 12 to your desired font size
+
+#     # Save the plot as a .png file using the GR backend
+#     # Plots.gr()
+#     # Plots.pgfplotsx()
+#     Plots.savefig(p, filename*"_plot.png")
+#     # Plots.savefig(p, filename*"_plot.tex")
+
+# end
+# function convergence_analysis(; ns, dts, dirname, solver_config, spatial=false, dt_const=2^-4, transient=false, n_const=2^7)
+#     println("Run convergence",)
+
+#     if (transient)
+#         # Time dim EOC
+#         el2s_L2 = Float64[]
+#         eh1s_L2 = Float64[]
+#         ehs_energy_L2 = Float64[]
+#         el2s_inf = Float64[]
+#         eh1s_inf = Float64[]
+#         ehs_energy_inf = Float64[]
+#         println("Run convergence tests with constant n = "*string(n_const))
+
+#         for dt in dts
+#             sol = Solver.run(n=n_const, dt=dt, solver_config=solver_config, vtkdirname=dirname)
+
+#             push!(el2s_L2, sol.el2s_L2)
+#             push!(eh1s_L2, sol.eh1s_L2)
+#             push!(ehs_energy_L2, sol.ehs_energy_L2)
+#             push!(el2s_inf, sol.el2s_inf)
+#             push!(eh1s_inf, sol.eh1s_inf)
+#             push!(ehs_energy_inf, sol.ehs_energy_inf)
+#         end
+#         generate_figures(dts, el2s_L2, eh1s_L2, ehs_energy_L2,
+#                          el2s_inf, eh1s_inf, ehs_energy_inf,
+#                          dirname, "dt")
+#     end
 
 
+#     if (spatial)
+#         # Spatial EOC
+#         el2s_L2 = Float64[]
+#         eh1s_L2 = Float64[]
+#         ehs_energy_L2 = Float64[]
+#         el2s_inf = Float64[]
+#         eh1s_inf = Float64[]
+#         ehs_energy_inf = Float64[]
+#         println("Run convergence tests with constant dt = "*string(dt_const))
+#         for n in ns
+#             sol = Solver.run(n=n, dt=dt_const, solver_config=solver_config, vtkdirname=dirname)
+#             push!(el2s_L2, sol.el2s_L2)
+#             push!(eh1s_L2, sol.eh1s_L2)
+#             push!(ehs_energy_L2, sol.ehs_energy_L2)
+#             push!(el2s_inf, sol.el2s_inf)
+#             push!(eh1s_inf, sol.eh1s_inf)
+#             push!(ehs_energy_inf, sol.ehs_energy_inf)
+#         end
+#         hs =  1 .// ns
+#         generate_figures(hs, el2s_L2, eh1s_L2, ehs_energy_L2,
+#                          el2s_inf, eh1s_inf, ehs_energy_inf,
+#                          dirname, "h")
+#     end
+# end
 
-end
+# function main_convergence()
 
-function main_convergence()
+#     dirname= "figures/cahn_hilliard_CutCIP/example"*string(Dates.now())
+#     println(dirname)
+#     mkpath(dirname)
 
-    dirname= "figures/cahn_hilliard_CutCIP/example"*string(Dates.now())
-    println(dirname)
-    mkpath(dirname)
+#     u_ex(x,t::Real) = sin(t)*(x[1]^2 + x[2]^2 - 1 )^3*sin(x[1])*cos(x[2])
+#     u_ex(t::Real) = x -> u_ex(x,t)
+#     exact_sol = Solver.man_sol(u_ex)
+#     circle = true
+#     solver_config = Solver.Config(exact_sol, circle)
 
-    u_ex(x,t::Real) = sin(t)*(x[1]^2 + x[2]^2 - 1 )^3*sin(x[1])*cos(x[2])
-    u_ex(t::Real) = x -> u_ex(x,t)
-    exact_sol = Solver.man_sol(u_ex)
-    circle = true
-    solver_config = Solver.Config(exact_sol, circle)
+#     dts = [2^-2,2^-3,2^-4,2^-5]
+#     ns = [2^2,2^3,2^4,2^5, 2^6]
+#     @time convergence_analysis( ns=ns, dts=dts, dirname=dirname, solver_config=solver_config, transient=true, spatial=true)
 
-    dts = [2^-2,2^-3,2^-4,2^-5]
-    ns = [2^2,2^3,2^4,2^5, 2^6]
-    @time convergence_analysis( ns=ns, dts=dts, dirname=dirname, solver_config=solver_config, transient=true, spatial=true)
+# end
 
-end
+# function main_simulation_test()
+#     dirname= "figures/cahn_hilliard_CutCIP/example"*string(Dates.now())
+#     println(dirname)
+#     mkpath(dirname)
 
-function main_simulation_test()
-    dirname= "figures/cahn_hilliard_CutCIP/example"*string(Dates.now())
-    println(dirname)
-    mkpath(dirname)
-
-    u_ex(x,t::Real) = sin(t)*(x[1]^2 + x[2]^2 - 1 )^3*sin(x[1])*cos(x[2])
-    u_ex(t::Real) = x -> u_ex(x,t)
-    exact_sol = Solver.man_sol(u_ex)
-    circle = true
-    solver_config = Solver.Config(exact_sol, circle)
-    @time Solver.run(n=2^5, dt=2^-3, solver_config=solver_config, vtkdirname=dirname)
-end
+#     u_ex(x,t::Real) = sin(t)*(x[1]^2 + x[2]^2 - 1 )^3*sin(x[1])*cos(x[2])
+#     u_ex(t::Real) = x -> u_ex(x,t)
+#     exact_sol = Solver.man_sol(u_ex)
+#     circle = true
+#     solver_config = Solver.Config(exact_sol, circle)
+#     @time Solver.run(n=2^5, dt=2^-3, solver_config=solver_config, vtkdirname=dirname)
+# end
+# """
 
 
 # main()
-main_convergence()
+# main_convergence()
