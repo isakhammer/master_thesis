b0484f75b0cc3db3454f05a4783263941cc71cdd
Andre Massing <andre.massing@gmail.com>
2023-05-01 19:17:54 +0200

    CH solver seems to work with chosen parameters

--- master_thesis/julia/cahn_hilliard/mvp_cahn-hilliard-laplace.jl
+++ master_thesis/julia/cahn_hilliard/mvp_cahn-hilliard-laplace.jl
@@ -3,19 +3,20 @@ using Gridap
 using Gridap.Algebra
 
 ## Cahn-hilliard
-ε = 1/100
+ε = 1/30
 # ε = 1
 # Gibb's potential
-ψ(u) = u*(1-u^2)
+ψ(u) = mean(u)*(1-mean(u)*mean(u))
+# ψ(u) = u*(1-u^2)
+# ψ(u) = u
 
-u_ex(x, t::Real) = cos(x[1])*cos(x[2])*exp(-4*t*ε^2)
+u_ex(x, t::Real) = cos(x[1])*cos(x[2])*exp(-(4*ε^2 + 2)*t)
 u_ex(t) = x -> u_ex(x,t)
 f_ex(x, t::Real) = 0 
 
-
 ## 
 L=2π
-n = 10
+n = 64
 h = L/n
 domain2D = (0, L, 0, L)
 partition2D = (n, n)
@@ -43,54 +44,65 @@ n_Γ = get_normal_vector(Γ)
 
 # M+ dt A
 γ = 1.5*order*( order+1)
-τ = ε^2/10
+τ = ε^2/100
 a(u,v) = ∫(u*v)*dΩ + τ*ε^2*( ∫(Δ(v)⊙Δ(u))dΩ
             + ∫(-mean(Δ(v))⊙jump(∇(u)⋅n_Λ) - mean(Δ(u))⊙jump(∇(v)⋅n_Λ) + (γ/h)⋅jump(∇(u)⋅n_Λ)⊙jump(∇(v)⋅n_Λ))dΛ
             + ∫(-Δ(v)⊙∇(u)⋅n_Γ - Δ(u)⊙∇(v)⋅n_Γ + (γ/h)⋅ ∇(u)⊙n_Γ⋅∇(v)⊙n_Γ )dΓ
         )
 
 # l(u, v) = ∫(τ*f*v + u*v)*dΩ
-l(u, v) = ∫(u*v)*dΩ
+l(u, v) =  ∫(u*v)*dΩ + τ * ( 
+            ∫(ψ(u)*Δ(v))*dΩ 
+          - ∫(ψ(mean(u))*jump(∇(v)⋅n_Λ))*dΛ
+          - ∫(ψ(u)*∇(v)⋅n_Γ )*dΓ
+          )
 l(u) = v -> l(u,v)
 
 createpvd(resultdir*"ch-solution") do pvd
+
+    ## time loop
+    t0 = 0.0
+    T = 1000*τ
+    Nt_max = convert(Int64, ceil((T - t0)/τ))
+    t = t0
+
+    # Initial data
+    u_dof_vals = (rand(Float64, num_free_dofs(U)) .-0.5)*2.0
+    # uh = interpolate_everywhere(u_ex(0),U)
+    uh = FEFunction(U, u_dof_vals)
+    pvd[t] = createvtk(Ω, resultdir*"ch-solution_$t"*".vtu",cellfields=["uh"=>uh, "u_ex"=>u_ex(t)])
+
+    println("========================================")
+    Nt = 1
+    t += τ
+    println("Solving Cahn-Hilliard with t0 = $t0, T = $T and time step τ = $τ with Nt_max = $Nt_max timesteps")
+    println("----------------------------------------")
 
     ## Set up linear algebra system
     A = assemble_matrix(a, U, V)
     lu = LUSolver()
 
-    ## time loop
-    t0 = 0.0
-    T = 100*τ
-    t = t0
-
-    println(t)
-    # u_dof_vals = rand(Float64, num_free_dofs(U))
-    uh = interpolate_everywhere(u_ex(0),U)
-    pvd[t] = createvtk(Ω, resultdir*"ch-solution_$t"*".vtu",cellfields=["uh"=>uh, "u_ex"=>u_ex(t)])
-
     # Solve for first time step
     b = assemble_vector(l(uh), V)
     op = AffineOperator(A, b)
-
     u_dof_vals = get_free_dof_values(uh)
     cache = solve!(u_dof_vals, lu, op)
     uh = FEFunction(U, u_dof_vals)
 
-    t += τ
     pvd[t] = createvtk(Ω, resultdir*"ch-solution_$t"*".vtu",cellfields=["uh"=>uh, "u_ex"=>u_ex(t)])
 
     # Remaining while loop
     while t < T
+        Nt += 1
+        t += τ
+        println("Solving Cahn-Hilliard for t = $t, step $(Nt)/$(Nt_max)")
+        println("----------------------------------------")
         b = assemble_vector(l(uh), V)
-
         u_dof_vals = get_free_dof_values(uh)
         op = AffineOperator(A, b)
         cache = solve!(u_dof_vals, lu, op, cache, false)
         uh = FEFunction(U, u_dof_vals)
 
-        t += τ
         pvd[t] = createvtk(Ω, resultdir*"ch-solution_$t"*".vtu",cellfields=["uh"=>uh, "u_ex"=>u_ex(t)])
-
     end
 end 
