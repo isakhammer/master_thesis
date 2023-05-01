1a527d49b7a488510b1847baf5f7cffadfa71ab0
Andre Massing <andre.massing@gmail.com>
2023-05-01 17:04:55 +0200

    Implemented implicit euler for 4th order parabolic problem

--- master_thesis/julia/cahn_hilliard/mvp_cahn-hilliard-laplace.jl
+++ master_thesis/julia/cahn_hilliard/mvp_cahn-hilliard-laplace.jl
@@ -3,14 +3,13 @@ using Gridap
 using Gridap.Algebra
 
 ## Cahn-hilliard
-# ε = 1/100
-ε = 1
+ε = 1/100
+# ε = 1
 # Gibb's potential
 ψ(u) = u*(1-u^2)
 
-u_ex(x, t::Real) = cos(x[1])*cos(x[2])*exp(-4*t/ε^2)
+u_ex(x, t::Real) = cos(x[1])*cos(x[2])*exp(-4*t*ε^2)
 u_ex(t) = x -> u_ex(x,t)
-
 f_ex(x, t::Real) = 0 
 
 
@@ -45,7 +44,7 @@ n_Γ = get_normal_vector(Γ)
 # M+ dt A
 γ = 1.5*order*( order+1)
 τ = ε^2/10
-a(u,v) = ∫(u*v)*dΩ + τ/ε^2*( ∫(Δ(v)⊙Δ(u))dΩ
+a(u,v) = ∫(u*v)*dΩ + τ*ε^2*( ∫(Δ(v)⊙Δ(u))dΩ
             + ∫(-mean(Δ(v))⊙jump(∇(u)⋅n_Λ) - mean(Δ(u))⊙jump(∇(v)⋅n_Λ) + (γ/h)⋅jump(∇(u)⋅n_Λ)⊙jump(∇(v)⋅n_Λ))dΛ
             + ∫(-Δ(v)⊙∇(u)⋅n_Γ - Δ(u)⊙∇(v)⋅n_Γ + (γ/h)⋅ ∇(u)⊙n_Γ⋅∇(v)⊙n_Γ )dΓ
         )
