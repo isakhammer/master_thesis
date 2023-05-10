747bdd8b1cb03c95a4e0b6613fb71cd085d87d3e
isakhammer <isakhammer@gmail.com>
2023-05-11 00:00:11 +0200

    Paramter sweep in CutCIP laplace

--- master_thesis/julia/biharmonic_experiments/biharmonic_CutCIP_laplace.jl
+++ master_thesis/julia/biharmonic_experiments/biharmonic_CutCIP_laplace.jl
@@ -1,4 +1,5 @@
 using Dates
+using Printf
 import Plots
 
 using LaTeXStrings
@@ -33,19 +34,20 @@ module Solver
     end
 
 
-    function run(; n, u_ex, dirname, L=1.11, grid_translation=0.0, ghost_penalty=true)
+    function run(; n, u_ex, dirname, L=1.11, δ=0.0, γ=10, γg1=5, γg2=0.1)
 
         order = 2
         u_ex, f, ∇u_ex, ∇Δu_ex = man_sol(u_ex)
 
-        # Background model
-        pmin = Point(-L+ grid_translation, -L)
-        pmax = Point(L + grid_translation, L)
+        # Background model (translated)
+        θ_δ = 0*( π/4 )
+        r_δ = δ*Point(cos(θ_δ),sin(θ_δ))
+        pmin = Point(-L, -L) + r_δ
+        pmax = Point(L , L) + r_δ
         bgorigin = ( pmin + pmax )/2
 
         R  = 1.0
-        println("Sim: order=$order, n=$n, bg LxL=$(round(L, digits=2))x$(round(L, digits=2)), bgorigin=($(round(bgorigin[1], digits=2)),$(round(bgorigin[2], digits=2))), disk R=$(round(R, digits=1))")
-
+        println("Sim: order=$order, n=$n, bg (L,L)=($(round(L, digits=2)),$(round(L, digits=2))), bgorigin=($(round(bgorigin[1], digits=2)),$(round(bgorigin[2], digits=2))), disk R=$(round(R, digits=1))")
         # Background model
         partition = (n,n)
         bgmodel = CartesianDiscreteModel(pmin, pmax, partition)
@@ -79,19 +81,6 @@ module Solver
         n_Γ = get_normal_vector(Γ)
         n_Λ = get_normal_vector(Λ)
         n_Fg = get_normal_vector(Fg)
-
-        # Define weak form
-        γ = 10*order*(order - 1)
-
-        # Ghost penalty parameter
-        # γg0 = γ
-        γg1 = 10/2
-        γg2 = 0.1
-
-        if ghost_penalty == false
-            γg1 = 0.0
-            γg2 = 0.0
-        end
 
         # Mesh size
         h = L/n
@@ -129,16 +118,8 @@ module Solver
                 + ∫(g_1⊙(-Δ(v) + (γ/h)*∇(v)⋅n_Γ)) * dΓ
                )
 
-        # Define of ghost penalties
-        if order >= 2
-            # g(u,v) = h^(-2)*(∫( (γg0/h)*jump(u)*jump(v)) * dFg +
-            #                   ∫( (γg1*h)*jump(n_Fg⋅∇(u))*jump(n_Fg⋅∇(v)) ) * dFg +
-            #                   ∫( (γg2*h^3)*jump_nn(u,n_Fg)*jump_nn(v,n_Fg) ) * dFg)
-            g(u,v) = h^(-2)*( ∫( (γg1*h)*jump(n_Fg⋅∇(u))*jump(n_Fg⋅∇(v)) ) * dFg +
-                              ∫( (γg2*h^3)*jump_nn(u,n_Fg)*jump_nn(v,n_Fg) ) * dFg)
-        else
-            println("Not supported order:", order)
-        end
+        g(u,v) = h^(-2)*( ∫( (γg1*h)*jump(n_Fg⋅∇(u))*jump(n_Fg⋅∇(v)) ) * dFg +
+                         ∫( (γg2*h^3)*jump_nn(u,n_Fg)*jump_nn(v,n_Fg) ) * dFg)
 
         A(u,v) = a_CIP(u,v) + g(u,v)
 
@@ -164,7 +145,7 @@ module Solver
 
         eh1 = sqrt(sum( ∫( e⊙e + ∇(e)⊙∇(e) )dΩ ))
 
-        vtkdirname =dirname*"/gp$(Int64(ghost_penalty))_order_"*string( order )*"_n_"*string(n)
+        vtkdirname =dirname*"/g_$(γ)_g1_$(γg1)_g2_$(γg2)_order_$(order)_n_$n"
         mkpath(vtkdirname)
 
         # Write out models and computational domains for inspection
@@ -207,7 +188,6 @@ function generate_figures(;ns, el2s, eh1s, ehs_energy, cond_numbers, ndofs, dirn
     open(filename*".tex", "w") do io
         pretty_table(io, data, header=header, backend=Val(:latex ), formatters = formatters )
     end
-
     minimal_header = ["n", "L2", "EOC", "H1", "EOC", "a_h", "EOC", "cond", "const", "ndofs"]
     data = hcat(ns, el2s,  eoc_l2, eh1s, eoc_eh1, ehs_energy, eoc_eh_energy, cond_numbers, cond_numbers.*hs.^4, ndofs)
     formatters = ( ft_printf("%.0f",[1,10]), ft_printf("%.2f",[3,5,7]), ft_printf("%.1E",[2,4,6,8,9]), ft_nonothing )
@@ -216,7 +196,6 @@ function generate_figures(;ns, el2s, eh1s, ehs_energy, cond_numbers, ndofs, dirn
     open(filename*".txt", "w") do io
         pretty_table(io, data, header=minimal_header, backend=:text, formatters=formatters)
     end
-
     # Initial plot with the first data series
     p = Plots.plot(hs, el2s, label=L"\Vert e \Vert_{L^2}", legend=:bottomright, xscale=:log2, yscale=:log2, minorgrid=true)
     Plots.scatter!(p, hs, el2s, primary=false)
@@ -237,8 +216,8 @@ function generate_figures(;ns, el2s, eh1s, ehs_energy, cond_numbers, ndofs, dirn
 
     # Save the plot as a .png file using the GR backend
     Plots.gr()
-    Plots.savefig(p, filename*"_plot.png")
     # Plots.pgfplotsx()
+    Plots.savefig(p, filename*"_plot.png")
     # Plots.savefig(p, filename*"_plot.tex")
 end
 
@@ -250,6 +229,7 @@ function convergence_analysis(; ns, dirname, u_ex)
     cond_numbers = Float64[]
     ndofs = Float64[]
 
+    println("Convergence test", ns)
     for n in ns
 
         sol = Solver.run(n=n, u_ex=u_ex, dirname=dirname)
@@ -264,45 +244,189 @@ function convergence_analysis(; ns, dirname, u_ex)
                      cond_numbers=cond_numbers, ndofs=ndofs, dirname=dirname)
 end
 
+
+# Custom struct to hold parameters, color, and data
+struct SimulationData
+    params::Tuple{Float64, Float64, Float64}
+    color::String
+    cond_numbers::Vector{Float64}
+    el2s::Vector{Float64}
+    eh1s::Vector{Float64}
+    ehs_energy::Vector{Float64}
+end
+
 function translation_test(; dirname, u_ex )
-    iterations = 100
-    x0 = 0
-    x1 = 0.2
-    L = 1.11 + x1
+    iterations = 5
+    δ1 = 0
+    δ2 = 0.2
+    L = 1.11 + δ2
     n=2^6
-    xs = LinRange(x0, x1, iterations)
+    δs = LinRange(δ1, δ2, iterations)
 
-    cond_numbers_gp = Float64[]
 
-    # EXPERIMENT WITH GHOST PENALTIES
-    println("\nTranslation test from x = $x0 to $x1;  iterations $iterations;  L = $L, n=$n")
-    for xi in xs
-        sol = Solver.run( n=n, u_ex=u_ex, dirname=dirname,
-                         grid_translation=xi, L=L, ghost_penalty=true)
-        push!(cond_numbers_gp, sol.cond_number)
+    function translation_solve(;δs, L, n, γ, γg1, γg2)
+        println("\nTranslation $δ1 to $δ2;  iterations $(length(δs)); L = $L, n=$n, γ=$γ, γg1=$γg1, γg2=$γg2")
+
+        el2s = Float64[]
+        eh1s = Float64[]
+        ehs_energy = Float64[]
+        cond_numbers = Float64[]
+        for δi in δs
+            sol = Solver.run( n=n, u_ex=u_ex, dirname=dirname,
+                             δ=δi,γ=γ, γg1=γg1, γg2=γg2, L=L)
+            push!(el2s, sol.el2)
+            push!(eh1s, sol.eh1)
+            push!(ehs_energy, sol.eh_energy)
+            push!(cond_numbers, sol.cond_number)
+        end
+        cond_numbers = [number > 1e23 ? 1e23 : number for number in cond_numbers] #ceiling cond numbers
+        return cond_numbers, el2s, eh1s, ehs_energy
     end
 
+    function run_simulations(param_list, δs, L, n)
+        results = Vector{SimulationData}()
 
-    # EXPERIMENT WITH NO GHOST PENALTIES
-    println("\nTranslation no ghost penalty test from x = $x0 to $x1;  iterations $iterations;  L = $L, n=$n")
-    cond_numbers = Float64[]
-    for xi in xs
-        sol = Solver.run( n=n, u_ex=u_ex, dirname=dirname,
-                         grid_translation=xi, L=L, ghost_penalty=false)
-        push!(cond_numbers, sol.cond_number)
+        for (params, color) in param_list
+            γ, γg1, γg2 = params
+            cond_numbers, el2s, eh1s, ehs_energy = translation_solve(δs=δs, L=L, n=n, γ=γ, γg1=γg1, γg2=γg2)
+            push!(results, SimulationData(params, color, cond_numbers, el2s, eh1s, ehs_energy))
+        end
+
+        return results
     end
-    cond_numbers = [number > 1e23 ? 1e23 : number for number in cond_numbers] #ceiling cond numbers
 
-    Plots.gr()
-    p = Plots.plot(xs, cond_numbers_gp, yscale=:log10, legend=:bottomright, label="Ghost Penalty", minorgrid=false)
-    Plots.scatter!(p, xs, cond_numbers_gp, primary=false, markerstrokealpha=0.4, markersize=3)
-    Plots.plot!(p, xs, cond_numbers, label="No Ghost Penalty")
-    Plots.scatter!(p, xs, cond_numbers, primary=false, markerstrokealpha=0.4, markersize=3)
-    Plots.xlabel!(p, "Translation")
-    Plots.ylabel!(p, "Condition number")
-    Plots.ylims!(p, (1e5, 1e25)) # Set the y-axis range
+    function create_plot_from_results(results, δs, dirname)
+        function sci_str(number)
+            if number == 0
+                return "\$ 0.0 \\cdot 10^{0} \$"
+            else
+                exp = round(log10(abs(number)))
+                mantissa = number / 10^exp
+                return @sprintf("\$%.1f \\cdot 10^{%d}\$", mantissa, exp)
+            end
+        end
 
-    Plots.savefig(p, dirname*"/translation_test.png")
+        Plots.gr()
+
+        # Plot condition numbers
+        p1 = Plots.plot(legend=:outertopright, legendtitle=L"(\gamma, \gamma_1, \gamma_2)", yscale=:log10, minorgrid=false)
+
+        # Plot L2 error
+        p2 = Plots.plot(legend=:outertopright, legendtitle=L"(\gamma, \gamma_1, \gamma_2)", yscale=:log10, minorgrid=false)
+
+        # Plot H1 error
+        p3 = Plots.plot(legend=:outertopright, legendtitle=L"(\gamma, \gamma_1, \gamma_2)", yscale=:log10, minorgrid=false)
+
+        # Plot Energy error
+        p4 = Plots.plot(legend=:outertopright, legendtitle=L"(\gamma, \gamma_1, \gamma_2)", yscale=:log10, minorgrid=false)
+
+        for sim_data in results
+            γ, γg1, γg2 = sim_data.params
+            label_text = L" %$(sci_str(γ)), %$(sci_str(γg1)), %$( sci_str(γg2) ) "
+            Plots.plot!(p1, δs, sim_data.cond_numbers, label=label_text, color=sim_data.color)
+            Plots.scatter!(p1, δs, sim_data.cond_numbers, primary=false, markerstrokealpha=0.4, markersize=3, color=sim_data.color)
+
+            Plots.plot!(p2, δs, sim_data.el2s, label=label_text, color=sim_data.color)
+            Plots.scatter!(p2, δs, sim_data.el2s, primary=false, markerstrokealpha=0.4, markersize=3, color=sim_data.color)
+
+            Plots.plot!(p3, δs, sim_data.eh1s, label=label_text, color=sim_data.color)
+            Plots.scatter!(p3, δs, sim_data.eh1s, primary=false, markerstrokealpha=0.4, markersize=3, color=sim_data.color)
+
+            Plots.plot!(p4, δs, sim_data.ehs_energy, label=label_text, color=sim_data.color)
+            Plots.scatter!(p4, δs, sim_data.ehs_energy, primary=false, markerstrokealpha=0.4, markersize=3, color=sim_data.color)
+        end
+
+        Plots.xlabel!(p1, L"$\delta$")
+        Plots.ylabel!(p1, L"$\kappa(A)$")
+        Plots.ylims!(p1, (1e5, 1e25))
+
+        Plots.xlabel!(p2, L"$\delta$")
+        Plots.ylabel!(p2, L"el2")
+
+        Plots.xlabel!(p3, L"$\delta$")
+        Plots.ylabel!(p3, L"eh1")
+
+        Plots.xlabel!(p4, L"$\delta$")
+        Plots.ylabel!(p4, L"e_ah")
+
+        Plots.savefig(p1, dirname*"/cond_trans.png")
+        Plots.savefig(p2, dirname*"/l2_trans.png")
+        Plots.savefig(p3, dirname*"/h1_trans.png")
+        Plots.savefig(p4, dirname*"/ah_trans.png")
+        return
+    end
+
+    # No penlaty check
+    param_list = [
+        ((10., 5., 0.1), "blue"),
+        ((10., 0., 0.), "red")
+    ]
+
+    plotdir = dirname*"/no_penalty"
+    mkpath(plotdir)
+    results = run_simulations(param_list, δs, L, n)
+    create_plot_from_results(results, δs, plotdir)
+
+
+    # Base params
+    σ = [10^6, 10^5, 10^4, 10^3, 10^2, 10^-2, 10^-3, 10^-4, 10^-5, 10^-6]
+
+    # Parameter Sweep γg2
+    param_list = [
+                  ((10., 5., 0.1 * σ[1]), "blue"),
+                  ((10., 5., 0.1 * σ[2]), "red"),
+                  ((10., 5., 0.1 * σ[3]), "green"),
+                  ((10., 5., 0.1 * σ[4]), "purple"),
+                  ((10., 5., 0.1 * σ[5]), "orange"),
+                  ((10., 5., 0.1 * σ[6]), "cyan"),
+                  ((10., 5., 0.1 * σ[7]), "brown"),
+                  ((10., 5., 0.1 * σ[8]), "magenta"),
+                  ((10., 5., 0.1 * σ[9]), "pink"),
+                  ((10., 5., 0.1 * σ[10]), "lime"),
+                 ]
+
+    plotdir = dirname*"/parameter_sweep_g2"
+    mkpath(plotdir)
+    results = run_simulations(param_list, δs, L, n)
+    create_plot_from_results(results, δs, plotdir)
+
+    # Parameter Sweep γg1
+    param_list = [
+                  ((10., 5. * σ[1] , 0.1), "blue"),
+                  ((10., 5. * σ[2] , 0.1), "red"),
+                  ((10., 5. * σ[3] , 0.1), "green"),
+                  ((10., 5. * σ[4] , 0.1), "purple"),
+                  ((10., 5. * σ[5] , 0.1), "orange"),
+                  ((10., 5. * σ[6] , 0.1), "cyan"),
+                  ((10., 5. * σ[7] , 0.1), "brown"),
+                  ((10., 5. * σ[8] , 0.1), "magenta"),
+                  ((10., 5. * σ[9] , 0.1), "pink"),
+                  ((10., 5. * σ[10], 0.1), "lime"),
+                 ]
+
+    plotdir = dirname*"/parameter_sweep_g1"
+    mkpath(plotdir)
+    results = run_simulations(param_list, δs, L, n)
+    create_plot_from_results(results, δs, plotdir)
+
+    # Parameter Sweep γg1 and γg2
+    param_list = [
+                  ((10., 5.0*σ[1] , 0.1 * σ[1] ), "blue"),
+                  ((10., 5.0*σ[2] , 0.1 * σ[2] ), "red"),
+                  ((10., 5.0*σ[3] , 0.1 * σ[3] ), "green"),
+                  ((10., 5.0*σ[4] , 0.1 * σ[4] ), "purple"),
+                  ((10., 5.0*σ[5] , 0.1 * σ[5] ), "orange"),
+                  ((10., 5.0*σ[6] , 0.1 * σ[6] ), "cyan"),
+                  ((10., 5.0*σ[7] , 0.1 * σ[7] ), "brown"),
+                  ((10., 5.0*σ[8] , 0.1 * σ[8] ), "magenta"),
+                  ((10., 5.0*σ[9] , 0.1 * σ[9] ), "pink"),
+                  ((10., 5.0*σ[10], 0.1 * σ[10]), "lime"),
+                 ]
+
+    plotdir = dirname*"/parameter_sweep_g1_g2"
+    mkpath(plotdir)
+    results = run_simulations(param_list, δs, L, n)
+    create_plot_from_results(results, δs, plotdir)
 
 end
 
@@ -310,20 +434,17 @@ function main()
 
     # %% Manufactured solution
     L, m, r = (1, 1, 1)
+    u_ex(x) = (x[1]^2 + x[2]^2  - 1)^2*100*cos(m*( 2π/L )*x[1])*cos(r*( 2π/L )*x[2])
 
-    # Examples that works badly
-    u_ex(x) = 100*sin(m*( 2π/L )*x[1])*cos(r*( 2π/L )*x[2])
-
-    resultdir= "figures/biharmonic_laplace_CutCIP/"*string(Dates.now())
+    resultdir= "figures/biharmonic_CutCIP/"*string(Dates.now())
     println(resultdir)
     mkpath(resultdir)
 
-    ns = [2^2, 2^3, 2^4, 2^5, 2^6, 2^7]
-    # ns = [2^2, 2^3, 2^4, 2^5, 2^6]
+    ns = [2^3, 2^4, 2^5, 2^6, 2^7]
 
-    # @time convergence_analysis( ns=ns,  dirname=resultdir, u_ex=u_ex,)
+    # @time convergence_analysis( ns=ns,  dirname=resultdir, u_ex=u_ex)
     @time translation_test(dirname=resultdir, u_ex=u_ex )
-end
 
+end
 
 main()
