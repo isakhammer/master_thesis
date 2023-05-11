ccba82b839a0ca5a1252735dacb35a5709f1108c
isakhammer <isakhammer@gmail.com>
2023-05-11 23:14:44 +0200

    Moved into a module

--- master_thesis/julia/biharmonic_CutFEM_experiments/translation_test.jl
+++ master_thesis/julia/biharmonic_CutFEM_experiments/translation_test.jl
@@ -1,33 +1,27 @@
-using LaTeXStrings
-using Latexify
+module TranslationTest
 
-# default_size = (800, 600)
-default_size = (400, 300)
+    using LaTeXStrings
+    using Latexify
+    import Plots
 
-Plots.pgfplotsx()
-endfix=".tex"
-# Plots.gr()
-# endfix=".png"
+    # default_size = (800, 600)
+    default_size = (400, 300)
+
+    # Plots.pgfplotsx()
+    # endfix=".tex"
+    Plots.gr()
+    endfix=".png"
 
 
-function sci_str(number)
-    if number == 0
-        return "\$ 0.0 \\cdot 10^{0} \$"
-    else
-        exp = round(log10(abs(number)))
-        mantissa = number / 10^exp
-        return @sprintf("\$%.1f \\cdot 10^{%d}\$", mantissa, exp)
+    function sci_str(number)
+        if number == 0
+            return "\$ 0.0 \\cdot 10^{0} \$"
+        else
+            exp = round(log10(abs(number)))
+            mantissa = number / 10^exp
+            return @sprintf("\$%.1f \\cdot 10^{%d}\$", mantissa, exp)
+        end
     end
-end
-
-function translation_test(; dirname, u_ex )
-    iterations = 1000
-    δ1 = 0
-    L = 1.11
-    n = 2^4
-    h = L/n
-    δ2 = sqrt(2)*h
-    δs = LinRange(δ1, δ2, iterations)
 
     function translation_solve(;δs, L, n, γ, γg1, γg2)
         println("\nTranslation $δ1 to $δ2;  iterations $(length(δs)); L = $L, n=$n, γ=$γ, γg1=$γg1, γg2=$γg2")
@@ -157,81 +151,92 @@ function translation_test(; dirname, u_ex )
 
     end
 
-    # No penlaty check
-    param_list = [
-        ((10., 5., 0.1), "blue"),
-        ((10., 0., 0.), "red")
-    ]
+    function translation_test(; dirname, u_ex )
+        iterations = 1000
+        δ1 = 0
+        L = 1.11
+        n = 2^4
+        h = L/n
+        δ2 = sqrt(2)*h
+        δs = LinRange(δ1, δ2, iterations)
 
-    results = run_simulations(param_list, δs, L, n)
 
-    prefix = "no_penalty"
-    create_plot_from_results(results, δs, dirname, prefix)
-    run_convergence(param_list, dirname, prefix)
+        # No penlaty check
+        param_list = [
+            ((10., 5., 0.1), "blue"),
+            ((10., 0., 0.), "red")
+        ]
 
-    return
+        results = run_simulations(param_list, δs, L, n)
 
-    # Base params
-    σ = [10^6, 10^5, 10^4, 10^3, 10^2, 10^-2, 10^-3, 10^-4, 10^-5, 10^-6]
+        prefix = "no_penalty"
+        create_plot_from_results(results, δs, dirname, prefix)
+        run_convergence(param_list, dirname, prefix)
 
-    # Parameter Sweep γg2
-    param_list = [
-                  ((10., 5., 0.1 * σ[1]), "blue"),
-                  ((10., 5., 0.1 * σ[2]), "red"),
-                  ((10., 5., 0.1 * σ[3]), "green"),
-                  ((10., 5., 0.1 * σ[4]), "purple"),
-                  ((10., 5., 0.1 * σ[5]), "orange"),
-                  ((10., 5., 0.1 * σ[6]), "cyan"),
-                  ((10., 5., 0.1 * σ[7]), "brown"),
-                  ((10., 5., 0.1 * σ[8]), "magenta"),
-                  ((10., 5., 0.1 * σ[9]), "pink"),
-                  ((10., 5., 0.1 * σ[10]), "lime"),
-                 ]
 
-    results = run_simulations(param_list, δs, L, n)
-    prefix = "sweep_g2"
-    create_plot_from_results(results, δs, dirname, prefix)
-    run_convergence(param_list, dirname, prefix)
+    end
 
-    # Parameter Sweep γg1
-    param_list = [
-                  ((10., 5. * σ[1] , 0.1), "blue"),
-                  ((10., 5. * σ[2] , 0.1), "red"),
-                  ((10., 5. * σ[3] , 0.1), "green"),
-                  ((10., 5. * σ[4] , 0.1), "purple"),
-                  ((10., 5. * σ[5] , 0.1), "orange"),
-                  ((10., 5. * σ[6] , 0.1), "cyan"),
-                  ((10., 5. * σ[7] , 0.1), "brown"),
-                  ((10., 5. * σ[8] , 0.1), "magenta"),
-                  ((10., 5. * σ[9] , 0.1), "pink"),
-                  ((10., 5. * σ[10], 0.1), "lime"),
-                 ]
+    function parameter_sweep()
+        # Base params
+        σ = [10^6, 10^5, 10^4, 10^3, 10^2, 10^-2, 10^-3, 10^-4, 10^-5, 10^-6]
 
-    results = run_simulations(param_list, δs, L, n)
-    prefix = "sweep_g1"
-    create_plot_from_results(results, δs, dirname, prefix)
-    run_convergence(param_list, dirname, prefix)
+        # Parameter Sweep γg2
+        param_list = [
+                      ((10., 5., 0.1 * σ[1]), "blue"),
+                      ((10., 5., 0.1 * σ[2]), "red"),
+                      ((10., 5., 0.1 * σ[3]), "green"),
+                      ((10., 5., 0.1 * σ[4]), "purple"),
+                      ((10., 5., 0.1 * σ[5]), "orange"),
+                      ((10., 5., 0.1 * σ[6]), "cyan"),
+                      ((10., 5., 0.1 * σ[7]), "brown"),
+                      ((10., 5., 0.1 * σ[8]), "magenta"),
+                      ((10., 5., 0.1 * σ[9]), "pink"),
+                      ((10., 5., 0.1 * σ[10]), "lime"),
+                     ]
 
-    # Parameter Sweep γg1 and γg2
-    param_list = [
-                  ((10., 5.0*σ[1] , 0.1 * σ[1] ), "blue"),
-                  ((10., 5.0*σ[2] , 0.1 * σ[2] ), "red"),
-                  ((10., 5.0*σ[3] , 0.1 * σ[3] ), "green"),
-                  ((10., 5.0*σ[4] , 0.1 * σ[4] ), "purple"),
-                  ((10., 5.0*σ[5] , 0.1 * σ[5] ), "orange"),
-                  ((10., 5.0*σ[6] , 0.1 * σ[6] ), "cyan"),
-                  ((10., 5.0*σ[7] , 0.1 * σ[7] ), "brown"),
-                  ((10., 5.0*σ[8] , 0.1 * σ[8] ), "magenta"),
-                  ((10., 5.0*σ[9] , 0.1 * σ[9] ), "pink"),
-                  ((10., 5.0*σ[10], 0.1 * σ[10]), "lime"),
-                 ]
+        results = run_simulations(param_list, δs, L, n)
+        prefix = "sweep_g2"
+        create_plot_from_results(results, δs, dirname, prefix)
+        run_convergence(param_list, dirname, prefix)
 
-    results = run_simulations(param_list, δs, L, n)
-    prefix = "sweep_g1_g2"
-    create_plot_from_results(results, δs, dirname, prefix)
-    run_convergence(param_list, dirname, prefix)
+        # Parameter Sweep γg1
+        param_list = [
+                      ((10., 5. * σ[1] , 0.1), "blue"),
+                      ((10., 5. * σ[2] , 0.1), "red"),
+                      ((10., 5. * σ[3] , 0.1), "green"),
+                      ((10., 5. * σ[4] , 0.1), "purple"),
+                      ((10., 5. * σ[5] , 0.1), "orange"),
+                      ((10., 5. * σ[6] , 0.1), "cyan"),
+                      ((10., 5. * σ[7] , 0.1), "brown"),
+                      ((10., 5. * σ[8] , 0.1), "magenta"),
+                      ((10., 5. * σ[9] , 0.1), "pink"),
+                      ((10., 5. * σ[10], 0.1), "lime"),
+                     ]
+
+        results = run_simulations(param_list, δs, L, n)
+        prefix = "sweep_g1"
+        create_plot_from_results(results, δs, dirname, prefix)
+        run_convergence(param_list, dirname, prefix)
+
+        # Parameter Sweep γg1 and γg2
+        param_list = [
+                      ((10., 5.0*σ[1] , 0.1 * σ[1] ), "blue"),
+                      ((10., 5.0*σ[2] , 0.1 * σ[2] ), "red"),
+                      ((10., 5.0*σ[3] , 0.1 * σ[3] ), "green"),
+                      ((10., 5.0*σ[4] , 0.1 * σ[4] ), "purple"),
+                      ((10., 5.0*σ[5] , 0.1 * σ[5] ), "orange"),
+                      ((10., 5.0*σ[6] , 0.1 * σ[6] ), "cyan"),
+                      ((10., 5.0*σ[7] , 0.1 * σ[7] ), "brown"),
+                      ((10., 5.0*σ[8] , 0.1 * σ[8] ), "magenta"),
+                      ((10., 5.0*σ[9] , 0.1 * σ[9] ), "pink"),
+                      ((10., 5.0*σ[10], 0.1 * σ[10]), "lime"),
+                     ]
+
+        results = run_simulations(param_list, δs, L, n)
+        prefix = "sweep_g1_g2"
+        create_plot_from_results(results, δs, dirname, prefix)
+        run_convergence(param_list, dirname, prefix)
+
+    end
+
 end
-
-
-
-
