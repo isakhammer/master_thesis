22b45ddb12634ecf6600801bf2bd719bfb239648
isakhammer <isakhammer@gmail.com>
2023-06-01 02:27:58 +0200

    Added Csv files

--- master_thesis/julia/biharmonic_CutFEM_experiments/eoc_test.jl
+++ master_thesis/julia/biharmonic_CutFEM_experiments/eoc_test.jl
@@ -53,13 +53,19 @@ function generate_figures(;ns, el2s, eh1s, ehs_energy, cond_numbers, ndofs, dirn
     open(filename_tex, "w") do io
         pretty_table(io, data; header=header, tf = tf_latex_modern, formatters=formatters)
     end
-    run(`sed -i '1d;$d' $filename_tex`) #removes \begin{table} env
+    run(`sed -i '1d;$d' $filename_tex`) #removes \begin{table} and \end{table} env
 
     minimal_header = ["h","n", "L2", "EOC", "H1", "EOC", "a_h", "EOC", "cond",  "ndofs"]
     pretty_table(data, header=minimal_header, formatters =formatters )
     open(filename*".txt", "w") do io
         pretty_table(io, data, header=minimal_header, formatters=formatters)
     end
+
+    # Producing a CSV file
+    CSV.write(filename*".csv", DataFrame(hs_str=hs_str, ns=ns, el2s=el2s,  eoc_l2=eoc_l2,
+                                         eh1s=eh1s, eoc_eh1=eoc_eh1,
+                                         ehs_energy=ehs_energy, eoc_eh_energy=eoc_eh_energy,
+                                         cond_numbers=cond_numbers, ndofs=ndofs), delim=',')
 
     # Initial plot with the first data series
     p = Plots.plot(hs, el2s, label=L"\Vert e \Vert_{L^2}", size=default_size, legend=:outertopright, xscale=:log2, yscale=:log2, minorgrid=true)
