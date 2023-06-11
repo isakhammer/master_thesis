2d0051f6af0695f6c697b3d183ac536ef69155dd
isakhammer <isakhammer@gmail.com>
2023-06-12 01:34:40 +0200

    Okey nice. Plots seems okay

--- master_thesis/latex/results/physical_CH_plot.jl
+++ master_thesis/latex/results/physical_CH_plot.jl
@@ -14,11 +14,14 @@ function main()
     data_circle = CSV.read("physical_CH_circle/sol.csv", DataFrame)
     data_flower = CSV.read("physical_CH_flower/sol.csv", DataFrame) # Make sure to correct the file path
 
-    p1 = plot(data_circle.ts, data_circle.e_L1_ts, size=default_size, xlabel=L"$t$", ylabel=L"$e_{L^1(\Omega)}$", legend=false, linecolor = :red)
-    plot!(p1, data_flower.ts, data_flower.e_L1_ts, linecolor = :blue)
+    its_circle = LinRange(1, length(data_circle.ts), length(data_circle.ts))
+    its_flower = LinRange(1, length(data_flower.ts), length(data_flower.ts))
 
-    p2 = plot(data_circle.ts[2:end], data_circle.Es[2:end],size=default_size, xscale=:log10, yscale=:log10, label = "Circle", xlabel=L"$t$", ylabel=L"$E(u_h)$", linecolor = :red)
-    plot!(p2, data_flower.ts[2:end], data_flower.Es[2:end], xscale=:log10, yscale=:log10, label = "Flower", linecolor = :blue)
+    p1 = plot(its_circle, data_circle.e_L1_ts, size=default_size, xlabel=L"$t/\tau$", ylabel=L"$e_{L^1(\Omega)}$", legend=false, linecolor = :red)
+    plot!(p1, its_flower, data_flower.e_L1_ts, linecolor = :blue)
+
+    p2 = plot(its_circle[2:end], data_circle.Es[2:end],size=default_size, xscale=:log10, yscale=:log10, label = "Circle", xlabel=L"$t/\tau$", ylabel=L"$E(u_h)$", linecolor = :red)
+    plot!(p2, its_flower[2:end], data_flower.Es[2:end], xscale=:log10, yscale=:log10, label = "Flower", linecolor = :blue)
 
     p = plot(p1, p2, layout = (1, 2)) # This will arrange p1 and p2 horizontally.
 
