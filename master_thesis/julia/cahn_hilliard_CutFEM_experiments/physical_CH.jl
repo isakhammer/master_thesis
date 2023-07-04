ed558c2cd936fcf9f4b8f86fdf8c4da1a5f74768
isakhammer <isakhammer@gmail.com>
2023-07-05 01:10:14 +0200

    Just plotted basic

--- master_thesis/julia/cahn_hilliard_CutFEM_experiments/physical_CH.jl
+++ master_thesis/julia/cahn_hilliard_CutFEM_experiments/physical_CH.jl
@@ -18,17 +18,12 @@ function main(;domain="flower")
     # Gibb's potential
     f(u) = mean(u)*(1 - mean(u)*mean(u))
     # f(u) = u*(1-u^2)
-    # f(u) = u
-
-    u_ex(x, t::Real) = cos(x[1])*cos(x[2])*exp(-(4*ε^2 + 2)*t)
-    u_ex(t) = x -> u_ex(x,t)
-    # f_ex(x, t::Real) = 0
 
     ##
-    L=2.70
+    L= 2.70
     n = 2^7
     h = 2*L/n
-    it = 1000
+    it = 40
     γ = 20
     τ = ε^2/60
     γg1 = 10
@@ -38,7 +33,6 @@ function main(;domain="flower")
     pmax = Point(L/2, L/2)
     partition = (n,n)
     bgmodel = CartesianDiscreteModel(pmin, pmax, partition)
-
 
     maindir = "figures/physical_CH_$domain"
     if isdir(maindir)
@@ -136,12 +130,15 @@ function main(;domain="flower")
 
     # Initial data
     u_dof_vals = (rand(Float64, num_free_dofs(U)) .-0.5)*2.0
+    # x_vals = range(0, stop=2, length=num_free_dofs(U))
+    # u_dof_vals = sin.(4π*x_vals)
+
     # uh = interpolate_everywhere(u_ex(0),U)
     u0 = FEFunction(U, deepcopy(u_dof_vals))
     uh = FEFunction(U, u_dof_vals)
     u0_L1 = abs( sum( ∫(u0)dΩ ) )
     pvd = Dict()
-    pvd[t] = createvtk(Ω, graphicsdir*"/sol_$t"*".vtu",cellfields=["uh"=>uh, "u_ex"=>u_ex(t)])
+    pvd[t] = createvtk(Ω, graphicsdir*"/sol_$t"*".vtu",cellfields=["uh"=>uh])
 
     # Adding initial plotting values
     δuhs = []
@@ -149,7 +146,7 @@ function main(;domain="flower")
     Es = Float64[]
     ts = Float64[]
     push!(ts, t)
-    E = sum( ∫((ε/2)*( ∇(uh)⋅∇(uh) ) + (1/ε)*(1/4)*((uh*uh - 1)*(uh*uh - 1))  )dΩ)
+    E = sum( ∫(( ∇(uh)⋅∇(uh) ) + (1/4)*((uh*uh - 1)*(uh*uh - 1))  )dΩ)
 
     push!(Es, E)
     # First step is not defined
@@ -194,7 +191,7 @@ function main(;domain="flower")
         uh0 = FEFunction(U, deepcopy( uh.free_values ))
 
         push!(Es, E)
-        pvd[t] = createvtk(Ω, graphicsdir*"/sol_$t"*".vtu",cellfields=["uh"=>uh, "u_ex"=>u_ex(t)])
+        pvd[t] = createvtk(Ω, graphicsdir*"/sol_$t"*".vtu",cellfields=["uh"=>uh])
     end
 
     # Construct pvd file
@@ -221,13 +218,26 @@ function main(;domain="flower")
     )
     YAML.write_file(maindir*"/parameters.yml", parameters)
 
-    # Normalize data
-    p1 = plot(ts, δuhs, label = "e_u", xlabel="t")
-    p2 = plot(ts[2:end], Es[2:end], xscale=:log10, yscale=:log10, label = "E(u)", xlabel="t")
-    savefig(p1,maindir*"/mass_cons.png")
-    savefig(p2,maindir*"/energy.png")
+    its = LinRange(1, length(ts), length(ts))
+
+    default_size = (800, 800)
+    p1 = plot(its[1:end-1], δuhs[2:end], size=default_size, xscale=:log10, legend=false, ylabel=L"$\delta u$", xlabel=L"$t/\tau$")
+    scatter!(its[1:end-1], δuhs[2:end], markersize = 2)
+    p2 = plot(its[1:end-1], Δuhs[2:end], size=default_size, yscale=:log10, legend=false, xscale=:log10, ylabel=L"$\Delta u$", xlabel=L"$t/\tau$")
+    scatter!(its[1:end-1], Δuhs[2:end], markersize = 2)
+    p3 = plot(its, Es, size=default_size, yscale=:log10, xscale=:log10, legend=false, ylabel=L"$E^m$", xlabel=L"$t/\tau$")
+    scatter!(its, Es, markersize = 2)
+    p4 = plot(its[1:end-1], Es[1:end-1] .- Es[2:end], size=default_size, legend=false,  yscale=:log10,  xscale=:log10,  xlabel=L"$t/\tau$", ylabel=L"$\Delta E^m$")
+    scatter!(its[1:end-1], Es[1:end-1] .- Es[2:end], markersize = 2)
+    # p4 = plot(its, E1s, size=default_size, yscale=:log10, xscale=:log10, legend=false, ylabel=L"$E_1^m$", xlabel=L"$t/\tau$")
+    # scatter!(its, E1s, markersize = 2)
+    # p5 = plot(its, E2s, size=default_size, yscale=:log10, xscale=:log10, legend=false, ylabel=L"$E_2^m$", xlabel=L"$t/\tau$")
+    # scatter!(its, E2s, markersize = 2)
+    p = plot(p1, p2, p3, p4, layout = (4, 1))
+    # p = plot(p1, p2, p3, p4, layout = (6, 1))
+    savefig(p,maindir*"/physical_CH_plot.pdf" )
 
 end
 
 main(domain="circle")
-main(domain="flower")
+# main(domain="flower")
